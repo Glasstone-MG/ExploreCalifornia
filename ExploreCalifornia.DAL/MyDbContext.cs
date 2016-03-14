e4ee4196e02bc29e4c3fd98ba0a64db0033cef19
S. Marcelo Volta <S. Marcelo Volta>
2016-03-14 19:49:04 -0300

    Chapter 4, Lesson 2. Completed with Connection String in the Web.config file

---
+++ ExploreCalifornia.DAL/MyDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+
+namespace ExploreCalifornia.Models
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext()
+            : base(ConfigurationManager.ConnectionStrings["ECConn"].ConnectionString )
+        {
+
+        }
+
+        public DbSet<Tour> Tours { get; set; }
+    }
+}
